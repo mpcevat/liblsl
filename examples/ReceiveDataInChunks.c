a8e10a6a3f5199049715fc4e9ffdb827051a12f9
Marc <mpcevat@users.noreply.github.com>
2024-01-30 09:27:16 +0100

    Create ReceiveDataInChunks.c
    ReceiveDataInChunks failed for ECG recorders with a different number of leads than the 32(?) assumed in the original coding, caused by a mismatch of the number of timestamps in lsl.pull_chunk_multiplexed

---
+++ examples/ReceiveDataInChunks.c
@@ -0,0 +1,42 @@
+using System;
+using LSL;
+
+namespace LSLExamples
+{
+    static class ReceiveDataInChunks
+    {
+        public static void Main(string[] args)
+        {
+            // wait until an EEG stream shows up
+            StreamInfo[] results = LSL.LSL.resolve_stream("type", "ExG");
+
+            // open an inlet, with post-processing enabled, and print meta-data
+            // Note: The use of post-processing makes it impossible to recover
+            // the original timestamps and is not recommended for applications
+            // that store data to disk.
+            using StreamInlet inlet = new StreamInlet(results[0],
+                postproc_flags: processing_options_t.proc_ALL);
+            results.DisposeArray();
+            System.Console.Write(inlet.info().as_xml());
+
+            // read samples
+            float[,] buffer = new float[512, 8];
+
+            // calculate the number of timestamps
+            int num_chans = inlet.info().channel_count();                         
+            int num_timestamps = buffer.Length / num_chans; 
+            double[] timestamps = new double[num_timestamps];
+
+            while (!Console.KeyAvailable)
+            {
+                int num = inlet.pull_chunk(buffer, timestamps);
+                for (int s = 0; s < num; s++)
+                {
+                    for (int c = 0; c < 8; c++)
+                        Console.Write("\t{0}", buffer[s, c]);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
